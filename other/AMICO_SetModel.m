11fd76889fab903dd22f289af750d98562c4dea9
Alessandro Daducci <alessandro.daducci@gmail.com>
2015-04-14 17:17:45 -0400

    Small typo

--- other/AMICO_SetModel.m
+++ other/AMICO_SetModel.m
@@ -2,23 +2,23 @@
 % Set the default parameters for each model
 %
 function AMICO_SetModel( model )
-	
-	global CONFIG
+
+    global CONFIG
 
     CONFIG.model   = [];
-    
-	CONFIG.OPTIMIZATION = [];
-	CONFIG.OPTIMIZATION.LS_param    = optimset('TolX',1e-4);
-	CONFIG.OPTIMIZATION.SPAMS_param = [];
+
+    CONFIG.OPTIMIZATION = [];
+    CONFIG.OPTIMIZATION.LS_param    = optimset('TolX',1e-4);
+    CONFIG.OPTIMIZATION.SPAMS_param = [];
 
     % Call the specific model constructor
     modelClass = str2func( ['AMICO_' upper(model)] );
     if exist([func2str(modelClass) '.m'],'file')
         CONFIG.model = modelClass();
     else
-        error( '[KERNELS_Generate] Model "%s" not recognized', model )
+        error( '[AMICO_SetModel] Model "%s" not recognized', model )
     end
-    
-    % set the folder where output maps are strores
+
+    % set the folder where output maps are strored
     CONFIG.OUTPUT_path  = fullfile(CONFIG.DATA_path,'AMICO',CONFIG.model.id);
 end
