ed1927784b4d0d0f01c79cf9d2d443f9ed08906b
David Romascano <david.romascano@epfl.ch>
2018-07-11 10:45:44 +0200

    Modified CylinderGPD to match output of Camino for R = [0.01E-6,0.1E-6,0.5E-6,1.0E-6,5.0E-6,10.0E-6]

--- amico/analytic_formulas.py
+++ amico/analytic_formulas.py
@@ -1,11 +1,12 @@
 import scipy.special
 import numpy as np
 
+_GAMMA_ = 2.6751525E8
 _am_ = scipy.special.jnp_zeros(1, 60)
 
 def compute_PGSE_Sum(lambda_, beta, delta, DELTA, diff):
     """Computes PGSE waveform term in eq 8 from [1].
-    
+        
     References
     ----------
     .. [1] Ianus et al. (2013) Gaussian phase distribution approximations for oscillating gradient spin echo diffusion MRI. JMR, 227: 25-34
@@ -17,9 +18,9 @@ def compute_PGSE_Sum(lambda_, beta, delta, DELTA, diff):
         that_sum += term1/term2 * (lambda_[n]*diff*delta - 1 + np.exp(-lambda_[n]*diff*delta) + np.exp(-lambda_[n]*diff*DELTA) - 0.5*np.exp(-lambda_[n]*diff*(DELTA-delta)) - 0.5*np.exp(-lambda_[n]*diff*(DELTA+delta)))
     return that_sum
 
-def CylinderGPD(diff,theta,phi,R,scheme):
-    """Analytic computation of the Cylinder signal for N diffusion gradients [1].
-
+def CylinderGPD(diff,theta,phi,R,scheme,gyro_ratio=_GAMMA_):
+    """Analytic computation of the Cylinder signal for N diffusion gradients of a PGSE protocol [1].
+        
     Attributes
     ----------
     diff : float
@@ -32,25 +33,25 @@ def CylinderGPD(diff,theta,phi,R,scheme):
         Cylinder radius (meters)
     scheme : Scheme object
         PGSE scheme object encoding diffusion gradient orientations, amplitudes, durations and separations
-        
+    gyro_ratio : float (optional)
+        Gyromagnetic ratio.
+    
     References
     ----------
     .. [1] Ianus et al. (2013) Gaussian phase distribution approximations for oscillating gradient spin echo diffusion MRI. JMR, 227: 25-34
     """
     n_samples = scheme.nS
     dir = scheme.raw[:,:3].copy()
-    for k in range(n_samples):
-        dir[k,:] /= np.linalg.norm(dir[k,:])
     modG = scheme.raw[:,3].copy()
     DELTA = scheme.raw[:,4].copy()
     delta = scheme.raw[:,5].copy()
     G = dir*np.tile(modG,(3,1)).T
-    GAMMA = 2.6751525E8
+    modG = np.linalg.norm(G,axis=1)
     t = DELTA-delta/3.
     unitGn = np.zeros(n_samples)
-    idx = (modG > 0)
-    cyl_vector = np.array([np.cos(phi) * np.sin(theta), np.sin(phi) * np.sin(theta), np.cos(theta)])
-    unitGn[idx] = np.dot(cyl_vector[None,:],G[idx].T)[0,:] / (modG[idx] * np.linalg.norm(cyl_vector))
+    idx = (modG > 0.0)
+    cyl_vector = np.array([np.cos(phi) * np.sin(theta), np.sin(phi) * np.sin(theta), np.cos(theta)])[:,None]
+    unitGn[idx] = np.dot(G[idx,:],cyl_vector)[:,0] / (modG[idx] * np.linalg.norm(cyl_vector))
     alpha = np.arccos(unitGn)
     lambda_ = (_am_/R)**2
     beta_factor = 2*(R/_am_)**2 / (_am_**2-1)
@@ -59,8 +60,8 @@ def CylinderGPD(diff,theta,phi,R,scheme):
         if this_sum[i] == 0:
             idx = (delta == delta[i]) * (DELTA == DELTA[i])
             this_sum[idx] = compute_PGSE_Sum(lambda_,beta_factor,delta[i],DELTA[i],diff)
-    Sperp = np.exp(-2 * GAMMA**2 * modG**2 * np.sin(alpha)**2 * this_sum)
-    Spar = np.exp(-t  * GAMMA**2 * delta**2 * modG**2 * np.cos(alpha)**2 * diff)
+    Sperp = np.exp(-2 * gyro_ratio**2 * modG**2 * np.sin(alpha)**2 * this_sum)
+    Spar = np.exp(-t  * gyro_ratio**2 * delta**2 * modG**2 * np.cos(alpha)**2 * diff)
     return Sperp * Spar
 
 def Zeppelin(diffPar,theta,phi,diffPerp,scheme):
