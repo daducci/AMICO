2f29e71196be1ec759403317494222f80654a264
ketograph <ketograph@github.com>
2019-03-13 09:29:43 +0100

    version bump to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amico',
-    version='1.0.1',
+    version='1.0.2',
     description='Accelerated Microstructure Imaging via Convex Optimization (AMICO)',
     author='Alessandro Daducci',
     author_email='alessandro.daducci@gmail.com',
