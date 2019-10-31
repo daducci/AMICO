7e85f638e5a9992e68c3af847823a0e32b35a525
ErickHernandezGutierrez <erick.hernandez@cimat.mx>
2019-10-31 16:07:24 +0100

    Removing checking for unsupported versions of COMMIT

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 from setuptools import setup, find_packages
 
-try:
-    import commit
-
-    commit_version = commit.___version__.split('.')
-    commit_version = [int(version_val) for version_val in commit_version]
-    if commit_version[0] == 1 and commit_version[1] < 3:
-        raise RuntimeError( 'AMICO is not compatible with the current version of COMMIT.\nPlease do the following:\n1. Uninstall COMMIT.\n2. Try to re-install AMICO.\n3. Install COMMIT v1.3.0 or above.' )
-except:
-    print('NOTE: If you are going to install COMMIT, make sure you install COMMIT v1.3.0 or above.')
-
 setup(
     name='amico',
     version='1.1.0',
