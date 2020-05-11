29a2ba15578c9bdc526df7e15f76183015c33bbc
frheault <francois.m.rheault@usherbrooke>
2020-05-11 09:42:23 -0400

    Added long description, same license as COMMIT, added to git ignore

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+description='Implementation of the linear framework for Accelerated Microstructure Imaging via Convex Optimization (AMICO) from diffusion MRI data'
+
 setup(
-    name='amico',
+    name='amico-daducci',
     version='1.2.2',
     description='Accelerated Microstructure Imaging via Convex Optimization (AMICO)',
+    long_description=description,
     author='Alessandro Daducci',
     author_email='alessandro.daducci@univr.it',
     url='https://github.com/daducci/AMICO',
     packages=find_packages(),
     setup_requires=['Cython==0.29.17', 'numpy==1.18.4'],
     package_data={'' : ['*.bin', 'directions/*.bin']},
-    install_requires=['dipy==1.1.1', 'scipy==1.0.1', 'spams==2.6.1'],
+    install_requires=['dipy==1.1.1', 'scipy==1.4.1', 'spams==2.6.1'],
 )
