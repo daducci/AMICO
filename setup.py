6a5d3a9eb9731b78bcb04a5c32f6777599ebe604
frheault <francois.m.rheault@usherbrooke>
2020-05-12 12:57:30 -0400

    Switch license and package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 description = 'Implementation of the linear framework for Accelerated Microstructure Imaging via Convex Optimization (AMICO) from diffusion MRI data'
 
-setup(name='amico-daducci',
+setup(name='dmri-amico',
       version='1.2.2',
       description='Accelerated Microstructure Imaging via Convex Optimization (AMICO)',
       long_description=description,
