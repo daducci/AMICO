f11e71e1ea519516a3b166d982dfdb3fc6e24260
frheault <francois.m.rheault@usherbrooke>
2020-05-26 18:07:23 -0400

    Switch to == x.y.*

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 description = 'Implementation of the linear framework for Accelerated Microstructure Imaging via Convex Optimization (AMICO) from diffusion MRI data'
 
 setup(name='dmri-amico',
-      version='1.2.3.2',
+      version='1.2.3.3',
       description='Accelerated Microstructure Imaging via Convex Optimization (AMICO)',
       long_description=description,
       author='Alessandro Daducci',
       author_email='alessandro.daducci@univr.it',
       url='https://github.com/daducci/AMICO',
       packages=find_packages(),
-      setup_requires=['numpy>=1.18'],
-      package_data={'': ['*.bin', 'directions/*.bin']},
-      install_requires=['numpy>=1.18', 'dipy>=1.1', 'scipy>=1.4', 'python-spams>=2.6.1'])
+      setup_requires=['numpy==1.18.*'],
+      install_requires=['numpy==1.18.*', 'dipy==1.1.*', 'scipy==1.4.*', 'python-spams==2.6.1.*'],
+      package_data={'': ['*.bin', 'directions/*.bin']})
