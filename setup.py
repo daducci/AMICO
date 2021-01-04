3fca7b046db6a849e6b3629b19eccf8fd503c20a
Mario Ocampo <ocampo.mario@gmail.com>
2021-01-04 18:17:27 +0100

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 description = 'Implementation of the linear framework for Accelerated Microstructure Imaging via Convex Optimization (AMICO) from diffusion MRI data'
 
 setup(name='dmri-amico',
-      version='1.2.7',
+      version='1.2.8',
       description='Accelerated Microstructure Imaging via Convex Optimization (AMICO)',
       long_description=description,
       author='Alessandro Daducci',
