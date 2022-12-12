4d4637c6547bd98c64886a0b609b49f486bc304d
nightwnvol <clorindo.notte@univr.it>
2022-12-12 12:00:53 +0100

    build: update dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,56 @@
-from setuptools import setup, find_packages
-
-# import details from amico/info.py
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import cyspams
 import sys
-sys.path.insert(0, './amico/')
-import info
+import os
 
-setup(name=info.NAME,
-      version=info.VERSION,
-      description=info.DESCRIPTION,
-      long_description=info.LONG_DESCRIPTION,
-      author=info.AUTHOR,
-      author_email=info.AUTHOR_EMAIL,
-      url=info.URL,
-      license=info.LICENSE,
-      packages=find_packages(),
-      install_requires=['packaging', 'wheel', 'numpy>=1.12', 'scipy>=1.0', 'dipy>=1.0', 'spams>=2.6.5.2', 'tqdm>=4.56.0', 'joblib>=1.0.1'],
-      package_data={'': ['*.bin', 'directions/*.bin']})
+include_dirs = []
+libraries = []
+library_dirs = []
+extra_compile_args = []
+
+# cyspams headers
+include_dirs.extend(cyspams.get_include())
+
+# OpenBLAS library (cyspams requirement)
+try:
+      openblas_dir = os.environ['OPENBLAS_DIR']
+except KeyError as err:
+      print(f"\033[31mKeyError: cannot find the {err} env variable\033[0m")
+      exit(1)
+
+if sys.platform.startswith('win32'):
+      include_dirs.extend([openblas_dir+'/include'])
+      libraries.extend(['libopenblas']) # .lib filenames
+      library_dirs.extend([openblas_dir+'/lib'])
+      extra_compile_args.extend(['/std:c++14'])
+if sys.platform.startswith('linux'):
+      include_dirs.extend([openblas_dir])
+      libraries.extend(['stdc++', 'openblas']) # library names (not filenames)
+      library_dirs.extend([openblas_dir])
+      extra_compile_args.extend(['-std=c++14'])
+if sys.platform.startswith('darwin'):
+      include_dirs.extend([openblas_dir])
+      libraries.extend(['stdc++', 'openblas']) # library names (not filenames)
+      library_dirs.extend([openblas_dir])
+      extra_compile_args.extend(['-std=c++14'])
+
+extensions = [
+      Extension(
+            'amico.models',
+            sources=['amico/models.pyx'],
+            include_dirs=include_dirs,
+            libraries=libraries,
+            library_dirs=library_dirs,
+            extra_compile_args=extra_compile_args
+      ),
+      Extension(
+            'amico.lut',
+            sources=['amico/lut.pyx']
+      )
+]
+
+setup(
+      url='https://github.com/daducci/AMICO', # NOTE this is to show the 'Home-page' field when run 'pip show dmri-amico' (open bug https://github.com/pypa/pip/issues/11221)
+      ext_modules=cythonize(extensions)
+)
