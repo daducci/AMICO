60cc15e114f8e553a9b2834c3a7fdbcf157417b8
nightwnvol <clorindo.notte@univr.it>
2023-01-23 11:20:05 +0100

    build: update dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import cyspams
 import sys
-import os
+from os import environ
 
 include_dirs = []
 libraries = []
@@ -14,25 +14,22 @@ include_dirs.extend(cyspams.get_include())
 
 # OpenBLAS library (cyspams requirement)
 try:
-      openblas_dir = os.environ['OPENBLAS_DIR']
+      openblas_dir = environ['OPENBLAS_DIR']
 except KeyError as err:
       print(f"\033[31mKeyError: cannot find the {err} env variable\033[0m")
       exit(1)
 
+include_dirs.extend([openblas_dir+'/include'])
+library_dirs.extend([openblas_dir+'/lib'])
+
 if sys.platform.startswith('win32'):
-      include_dirs.extend([openblas_dir+'/include'])
       libraries.extend(['libopenblas']) # .lib filenames
-      library_dirs.extend([openblas_dir+'/lib'])
       extra_compile_args.extend(['/std:c++14'])
 if sys.platform.startswith('linux'):
-      include_dirs.extend([openblas_dir])
       libraries.extend(['stdc++', 'openblas']) # library names (not filenames)
-      library_dirs.extend([openblas_dir])
       extra_compile_args.extend(['-std=c++14'])
 if sys.platform.startswith('darwin'):
-      include_dirs.extend([openblas_dir])
       libraries.extend(['stdc++', 'openblas']) # library names (not filenames)
-      library_dirs.extend([openblas_dir])
       extra_compile_args.extend(['-std=c++14'])
 
 extensions = [
