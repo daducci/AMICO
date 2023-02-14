fc0758eb9fb99ddb9db1eb721940c353d4df46b5
nightwnvol <clorindo.notte@univr.it>
2023-02-14 14:11:40 +0100

    build: add 'wheel' to setup dependencies

--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ setup(name=info.NAME,
       url=info.URL,
       license=info.LICENSE,
       packages=find_packages(),
-      install_requires=['packaging', 'wheel', 'numpy>=1.12', 'scipy>=1.0', 'dipy>=1.0', 'spams>=2.6.5.2', 'tqdm>=4.56.0', 'joblib>=1.0.1'],
+      setup_requires=['wheel'],
+      install_requires=['packaging', 'numpy>=1.12', 'scipy>=1.0', 'dipy>=1.0', 'spams>=2.6.5.2', 'tqdm>=4.56.0', 'joblib>=1.0.1'],
       package_data={'': ['*.bin', 'directions/*.bin']})
