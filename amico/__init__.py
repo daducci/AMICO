3b5215bd6c427b2e65dcde97181266c7d61bd329
nightwnvol <clorindo.notte@univr.it>
2023-12-12 13:55:22 +0100

    refactor: replace relative imports

--- amico/__init__.py
+++ amico/__init__.py
@@ -1,10 +1,10 @@
-from .core import Evaluation, setup
-from .util import set_verbose, get_verbose
-from . import core
-from . import scheme
-from . import lut
-from . import models
-from . import util
+from amico.core import Evaluation, setup
+from amico.util import set_verbose, get_verbose
+from amico import core
+from amico import scheme
+from amico import lut
+from amico import models
+from amico import util
 
 try:
     from importlib.metadata import version
